07f22f60e406ec943d613165b20f1185c82a49d1
Tom <tom@broadwood.dev>
2021-12-18 18:23:01 +1000

    Added animations package, Gift card widget now has all required fields as per FR.2, Image now has a placeholder

--- flutter_business_mobile/lib/gift-cards/common/widgets/gift_card.widget.dart
+++ flutter_business_mobile/lib/gift-cards/common/widgets/gift_card.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_business_mobile/gift-cards/common/classes/gift_card.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class GiftCardWidget extends StatelessWidget {
   final GiftCard giftCard;
@@ -8,11 +9,50 @@ class GiftCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const TextStyle sub = TextStyle(color: Colors.black38);
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
-          children: [Text(giftCard.brand)],
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: FadeInImage.memoryNetwork(
+                image: giftCard.image,
+                placeholder: kTransparentImage,
+                width: 50,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      giftCard.brand,
+                      style: TextStyle(),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      giftCard.discount.toString(),
+                      style: sub,
+                    ),
+                    Text(
+                      giftCard.vendor,
+                      style: sub,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Details'),
+              ),
+            )
+          ],
         ),
       ),
     );
--- flutter_business_mobile/lib/gift-cards/gift_cards.dart
+++ flutter_business_mobile/lib/gift-cards/gift_cards.dart
@@ -3,6 +3,7 @@ import 'package:flutter_business_mobile/gift-cards/common/classes/gift_card.dart
 import 'package:flutter_business_mobile/gift-cards/common/widgets/gift_card.widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'common/providers.dart';
 
 class GiftCards extends StatelessWidget {
@@ -14,12 +15,22 @@ class GiftCards extends StatelessWidget {
         final AsyncValue<List<GiftCard>> getGiftCards = ref.watch(giftCardProvider);
         return getGiftCards.when(
           data: (List<GiftCard> data) {
-            return ListView.builder(
-              itemBuilder: (BuildContext listViewContext, int index) {
-                final GiftCard giftCard = data[index];
-                return GiftCardWidget(giftCard: giftCard);
-              },
-              itemCount: data.length,
+            return AnimationLimiter(
+              child: ListView.builder(
+                itemBuilder: (BuildContext listViewContext, int index) {
+                  final GiftCard giftCard = data[index];
+                  return AnimationConfiguration.staggeredList(
+                    position: index,
+                    duration: const Duration(milliseconds: 250),
+                    child: SlideAnimation(
+                      child: FadeInAnimation(
+                        child: GiftCardWidget(giftCard: giftCard),
+                      ),
+                    ),
+                  );
+                },
+                itemCount: data.length,
+              ),
             );
           },
           loading: () {
