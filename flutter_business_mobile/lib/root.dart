10fe79fa509e107b27b0da1bc4950cc4ae404108
Tom <tom@broadwood.dev>
2021-12-18 17:34:04 +1000

    Added badges package, Cart icon now shows how many items the cart contains

--- flutter_business_mobile/lib/root.dart
+++ flutter_business_mobile/lib/root.dart
@@ -1,4 +1,6 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_business_mobile/gift-cards/common/classes/gift_card.dart';
 import 'package:flutter_business_mobile/gift-cards/gift_cards.dart';
 import 'package:flutter_business_mobile/providers.dart';
 import 'package:flutter_business_mobile/shopping-cart/shopping_cart.dart';
@@ -16,27 +18,31 @@ class Root extends StatelessWidget {
       builder: (BuildContext context, WidgetRef ref, Widget? child) {
         final int index = ref.watch(screenProvider);
         return Scaffold(
-          appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-            title: Text('Flutter Business Mobile'),
-          ),
+          appBar: AppBar(title: const Text('Flutter Business Mobile')),
           body: _screens[index],
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: index,
-            items: const [
-              BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Gift Cards'),
-              BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Shopping Cart'),
+            items: [
+              const BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: 'Gift Cards'),
+              BottomNavigationBarItem(
+                  icon: Consumer(
+                    builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                      final int length = ref.watch(cartProvider).length;
+                      return Badge(
+                        badgeContent: Text(length.toString(), style: const TextStyle(color: Colors.white)),
+                        showBadge: length > 0,
+                        elevation: 0,
+                        shape: BadgeShape.circle,
+                        child: const Icon(Icons.shopping_cart),
+                      );
+                    },
+                  ),
+                  label: 'Shopping Cart'),
             ],
-            onTap: (index) {
+            onTap: (int index) {
               ref.read(screenProvider.notifier).state = index;
             },
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ), // This trailing comma makes auto-formatting nicer for build methods.
         );
       },
     );
