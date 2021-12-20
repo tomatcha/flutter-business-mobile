3f93c1a82a5a81808b99cadddeae70b2e807463a
Tom <tom@broadwood.dev>
2021-12-20 14:00:26 +1000

    Removed unused import, Left TODO

--- flutter_business_mobile/test/widget_test.dart
+++ flutter_business_mobile/test/widget_test.dart
@@ -1,11 +1,4 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter/material.dart';
-import 'package:flutter_business_mobile/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -14,7 +7,7 @@ import 'package:flutter_business_mobile/main.dart';
 void main() {
   testWidgets('Test Main Screens', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ProviderScope(child: MyApp()));
+    await tester.pumpWidget(const ProviderScope(child: MyApp())); // TODO HTTP requests don't work in tests
 
     await tester.tap(find.byIcon(Icons.card_giftcard));
     await tester.pump();
